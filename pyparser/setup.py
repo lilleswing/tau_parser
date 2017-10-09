a61f47ae9ef373bb934cfa9000b921525e527ac9
leswing <lilleswing@gmail.com>
2017-10-09 10:43:35 -0400

    single file

--- pyparser/setup.py
+++ pyparser/setup.py
@@ -1,4 +1,10 @@
 from distutils.core import setup
 import py2exe
+import sys
+import os
 
-setup(console=['parse_it.py'])
+setup(
+        options = {'py2exe': {'bundle_files':1, 'compressed': True}},
+        console=['parse_it.py'],
+        zipfile= None
+)
